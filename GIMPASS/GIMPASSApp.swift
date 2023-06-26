cd9e28dd3ba5dc0c9abb778bb44d8563d5e9422d
Mori <jardenlee@163.com>
2023-06-26 15:51:19 +0800

    Initial Commit

---
+++ GIMPASS/GIMPASSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIMPASSApp.swift
+//  GIMPASS
+//
+//  Created by MORI on 26/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct GIMPASSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
